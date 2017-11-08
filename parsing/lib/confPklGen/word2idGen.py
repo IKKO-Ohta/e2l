29c3518f0d56622b0efd1f224e971891f0a4193b
IKKO-Ohta <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-11-08 16:11:07 +0900

    word2idGeForOracle

--- parsing/lib/confPklGen/word2idGen.py
+++ parsing/lib/confPklGen/word2idGen.py
@@ -1,17 +1,56 @@
-
+import sys
 import glob
 import pickle
 
-mrgs = glob.glob("../auto/Penn_conbined_wsj/*/*mrg")
-m = {}
+oracles = glob.glob("../../auto/Penn_Oracle_split/*/*.oracle")
 
-for mrg in mrgs:
-    with open(mrg) as f:
+def oracle_load(oracle_path):
+    """
+    オラクルファイルを受け取って、confに代入できるよう整形する
+    featureとラベルが返る
+    1file-in,1datum-out
+    """
+    def str_eval(s):
+        return s[1:len(s) - 1].split(", ")
+
+    def oracle_split(lists_line):
+        div = lists_line.find("][")
+        stack = lists_line[:div + 1]
+        stack = str_eval(stack)
+        buffer = lists_line[div + 1:]
+        buffer = str_eval(buffer)
+        return [x for x in stack if x != " " or not x], [x for x in buffer if x != " " or not x]
+
+    feature, label = [], []
+    with open(oracle_path, "r") as f:
+        """
+        以下はoracle形式のつじつま合わせ
+        """
+        f.readline()
+        cnt = 0
         for line in f:
-            line = line.split("\t")
-            if len(line) <= 1:
-                continue
-            m[line[1]] = line[3]
+            if "][" in line:
+                line = line.rstrip()
+                line = oracle_split(line)
+                feature.append(line)
+                cnt += 1
+            elif line == "\n": # EOS
+                break
+            else:
+                line = line.rstrip()
+                label.append(line)
+                cnt += 1
+    return feature, label
 
-with open("../model/word2id.pkl", "wb") as f:
-    pickle.dump(m, f)
+word2id, cnt = {}, 0
+for oracle in oracles:
+    features, _ = oracle_load(oracle)
+    features = [feature[0] for feature in features]
+    for feature in features:
+        for elem in feature:
+            if not elem in word2id.keys():
+                word2id[elem] = cnt
+                cnt += 1
+
+with open("../../model/word2id.pkl","wb") as f:
+    pickle.dump(word2id,f)
