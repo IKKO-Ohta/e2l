4f1cc03aaf787e21b9781134bc71005920b57642
IKKO-Ohta <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-11-20 15:21:52 +0900

    typo

--- parsing/lib/divnull/pklcheck.py
+++ parsing/lib/divnull/pklcheck.py
@@ -2,12 +2,12 @@ import pickle
 import glob
 
 pkls = glob.glob("../../auto/preprocessed/13/*.pkl")
-for pkl in pkls:
-    with open(pkl,"br") as f:
+for pklName in pkls:
+    with open(pklName,"br") as f:
         pkl = pickle.load(f)
         pkl = pkl[0]
         his,buf,stk = pkl[0],pkl[1],pkl[2]
         if not 0 <= his <= 2:
             print("found..")
-            print(pkl)
+            print(pklName)
             print(his)
