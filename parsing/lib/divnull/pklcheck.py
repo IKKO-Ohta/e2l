e746f862f3a904b64be7deff42073fd59f6901fd
IKKO-Ohta <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-11-20 15:38:40 +0900

    range

--- parsing/lib/divnull/pklcheck.py
+++ parsing/lib/divnull/pklcheck.py
@@ -2,14 +2,14 @@ import pickle
 import sys
 import glob
 
-num = sys.argv[1]
-pkls = glob.glob("../../auto/preprocessed/{0}/*.pkl".format(num))
-for pklName in pkls:
-    with open(pklName,"br") as f:
-        pkl = pickle.load(f)
-        pkl = pkl[0]
-        his,buf,stk = pkl[0],pkl[1],pkl[2]
-        if not 0 <= his <= 2:
-            print("found..")
-            print(pklName)
-            print(his)
+for num in range(10,24):
+    pkls = glob.glob("../../auto/preprocessed/{0}/*.pkl".format(num))
+    for pklName in pkls:
+        with open(pklName,"br") as f:
+            pkl = pickle.load(f)
+            pkl = pkl[0]
+            his,buf,stk = pkl[0],pkl[1],pkl[2]
+            if not 0 <= his <= 2:
+                print("found..")
+                print(pklName)
+                print(his)
